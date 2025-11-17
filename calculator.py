756aaf18a872048ceadd6e57ca06a6bb0db3def6
kadenblu <kking.blue@icloud.com>
2025-11-16 19:07:51 -0600

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ValueError("invalid values for logarithm")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("invalid values for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
 
 
